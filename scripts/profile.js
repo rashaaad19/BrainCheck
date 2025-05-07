6402f2cbf58d192b7e1f334da985ffcaf19fc491
rashaaad19 <egyoussef_2000@hotmail.com>
2025-05-07 17:37:06 +0300

    feat: added structure for profile page

---
+++ scripts/profile.js
@@ -0,0 +1,30 @@
+import { getCurrentUserDoc } from "../services/firestore_queries_service.js";
+
+//get user ID from local storage
+const userID = localStorage.getItem('userId');
+//fetch the user document from firestore
+const userData = await getCurrentUserDoc(userID);
+console.log(userData)
+
+//Element selectors
+const userName = document.getElementById('userName');
+const userEmail = document.getElementById('userEmail');
+const userFullName = document.getElementById('userFullName');
+const userRole = document.getElementById('userRole');
+const userImg = document.getElementById('userImg');
+
+//Update the text of elements with user information
+userName.innerText = userData.displayName;
+userEmail.innerText = userData.email;
+userFullName.innerText = userData.displayName;
+userRole.innerText = userData.role;
+
+//conditionally changing profile image based on user role
+if(userData.role==='teacher'){
+    userImg.src = '../images/teacher.png'
+    console.log('teacher')
+}
+if(userData.role==='student'){
+    userImg.src = '../images/pupil.png'
+}
+
