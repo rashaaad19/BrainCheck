74bb05fcdf5b6ec02875cf1fba1d807c4be13dfc
Hadeer Mohamed <hadeerelgebaly77@gmail.com>
2025-05-10 14:21:18 +0300

    style: added dynamic images for each subject card and added hover styles

--- classes/Exam.js
+++ classes/Exam.js
@@ -1,37 +1,44 @@
 import { getAllSubjectsData } from "../services/firestore_queries_service.js";
 export class Exam {
-    constructor(id, name, duration, description, passingGrade, questions = []) {
-        this.id = id;
-        this.subjectName = name;
-        this.duration = duration;
-        this.description = description;
-        this.passingGrade = passingGrade;
-        this.questions = questions;
-    }
-    addQuestions(questionsArray) {
-        this.questions = questionsArray
-    }
-    isPassing(score) {
-        return score >= this.passingGrade
-    }
-    getDurationHours() {
-        return (this.duration / 60).toFixed(1);
-    }
+  constructor(
+    id,
+    name,
+    duration,
+    description,
+    passingGrade,
+    img,
+    questions = []
+  ) {
+    this.id = id;
+    this.subjectName = name;
+    this.duration = duration;
+    this.description = description;
+    this.passingGrade = passingGrade;
+    this.questions = questions;
+    this.img = img;
+  }
+  addQuestions(questionsArray) {
+    this.questions = questionsArray;
+  }
+  isPassing(score) {
+    return score >= this.passingGrade;
+  }
+  getDurationHours() {
+    return (this.duration / 60).toFixed(1);
+  }
 
-    shuffleQuestions() {
-        const arr = this.questions;
-        for (let i = arr.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [arr[i], arr[j]] = [arr[j], arr[i]];
-        }
-        this.questions = arr;
+  shuffleQuestions() {
+    const arr = this.questions;
+    for (let i = arr.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [arr[i], arr[j]] = [arr[j], arr[i]];
     }
-
+    this.questions = arr;
+  }
 }
 
 // const subjects = await getAllSubjectsData();
 // const subjectClasses = subjects.map(item => new Exam(item.id, item.subjectName, item.duration, item.description, item.passingGrade));
-
 
 // console.log(subjectClasses)
 // console.log(subjectClasses[0].isPassing(40))
--- scripts/home.js
+++ scripts/home.js
@@ -5,6 +5,8 @@ const container = document.getElementsByClassName("subjects-container")[0];
 console.log("Home js");
 //fetch subjects data and assign each subject as Exam instance
 const subjects = await getAllSubjectsData();
+console.log(subjects);
+
 const subjectClasses = subjects.map(
   (item) =>
     new Exam(
@@ -12,54 +14,58 @@ const subjectClasses = subjects.map(
       item.subjectName,
       item.duration,
       item.description,
-      item.passingGrade
+      item.passingGrade,
+      item.img
     )
 );
-
+console.log(subjectClasses);
 const subjectButtonHandler = async (id) => {
-    //find the selected class item based on user click
-    const selectedSubject = subjectClasses.find(subject => subject.id === id)
-    // Save the raw data (not the class instance) to localStorage
-    localStorage.setItem('selectedSubjectData', JSON.stringify(selectedSubject));
-    //navigate to exam page with custom subjectId query
-    window.location.href = `/pages/exam-page.html?subjectId=${selectedSubject.id}`
-}
+  //find the selected class item based on user click
+  const selectedSubject = subjectClasses.find((subject) => subject.id === id);
+  // Save the raw data (not the class instance) to localStorage
+  localStorage.setItem("selectedSubjectData", JSON.stringify(selectedSubject));
+  //navigate to exam page with custom subjectId query
+  window.location.href = `/pages/exam-page.html?subjectId=${selectedSubject.id}`;
+};
 
 //courses based on firestore
 subjectClasses.map((subject) => {
   const container = document.querySelector(".course");
 
   const card = document.createElement("div");
-  card.className = "subjects-container  flex flex-col justify-between card bg-white p-relative rad-10 p-10 gap-3";
-    let subjectnamedev=document.createElement("div")
-    subjectnamedev.className="flex flex-col items-start p-10 min-h-[150px]"
-    subjectnamedev.style.backgroundImage = "url('../images/Macroeconomics.jpg')";
-    subjectnamedev.style.backgroundSize = "cover"; 
-    subjectnamedev.style.backgroundRepeat = "no-repeat";
-    subjectnamedev.style.backgroundPosition = "center";
-    subjectnamedev.style.borderRadius="10px";
+  card.className =
+    "subjects-container  flex flex-col justify-between card bg-white p-relative rad-10 p-10 gap-3";
+  let subjectnamedev = document.createElement("div");
+  subjectnamedev.className = "flex flex-col items-start  min-h-[150px]";
+  subjectnamedev.style.backgroundImage = `url(${subject.img})`;
+  subjectnamedev.style.backgroundSize = "cover";
+  subjectnamedev.style.backgroundRepeat = "no-repeat";
+  subjectnamedev.style.backgroundPosition = "center";
+  subjectnamedev.style.borderRadius = "10px";
   subjectnamedev.innerHTML = `
+   <div class="courses flex flex-col items-start p-10 min-h-[150px] m-0">
+     <h3 class=" no fw-600 text-xl text-start min-h-10" style='color: rgb(0, 0, 163)' >${subject.subjectName}</h3>
+      <h3 class=" no text-xs line-1-6  "  style="color:white; padding-top:7px ">${subject.description}</h3>
    
-      <h3 class="fw-600 fs-18 text-start min-h-10" style='color:darkblue' >${subject.subjectName}</h3>
-      <h3 class="c-grey fs-14 line-1-6 pt-5 "  style="padding-top: 7px">${subject.description}</h3>
-    
+   </div>
   
+ 
   `;
-  card.appendChild(subjectnamedev)
+
+  card.appendChild(subjectnamedev);
 
   const btn = document.createElement("div");
-  btn.className = " flex justify-center start-btn mt-10 fw-500 c-blue pointer p-6";
-  const startbtn=document.createElement("button")
-  startbtn.className="btn btn-outline btn-success p-15 m-3"
-  startbtn.innerText ="Start Exam"
+  btn.className =
+    " flex justify-center start-btn mt-10 fw-500 c-blue pointer p-6";
+  const startbtn = document.createElement("button");
+  startbtn.className = " start ";
 
- 
+  startbtn.innerText = "Start Exam";
+
   startbtn.addEventListener("click", () => subjectButtonHandler(subject.id));
 
- btn.appendChild(startbtn);
+  btn.appendChild(startbtn);
   card.appendChild(btn);
-
 
   container.appendChild(card);
 });
-
