4c5fcea1e739634f935a885523e29dfc0403c02b
Hadeer Mohamed <hadeerelgebaly77@gmail.com>
2025-05-13 16:21:32 +0300

    final

--- scripts/home.js
+++ scripts/home.js
@@ -5,7 +5,7 @@ import {
   getCurrentUserDoc,
 } from "../services/firestore_queries_service.js";
 
-const logoutBtn = document.getElementsByClassName('logOut-btn')[0];
+const logoutBtn = document.getElementsByClassName("logOut-btn")[0];
 const userId = localStorage.getItem("userId");
 
 const currentUser = await getCurrentUserDoc(userId);
@@ -42,39 +42,43 @@ subjectClasses.map((subject) => {
   const container = document.querySelector(".course");
   const card = document.createElement("div");
   card.className =
-    "subjects-container  flex flex-col justify-between card bg-white p-relative rad-10 p-10 gap-3";
+    "subjects-container  flex flex-col justify-between card bg-white p-relative rad-10 pb-15";
   let subjectnamedev = document.createElement("div");
-  subjectnamedev.className = "flex flex-col items-start  min-h-[150px]";
+  subjectnamedev.className = "flex flex-col items-start  min-h-[250px]";
   subjectnamedev.style.backgroundImage = `url(${subject.img})`;
   subjectnamedev.style.backgroundSize = "cover";
   subjectnamedev.style.backgroundRepeat = "no-repeat";
   subjectnamedev.style.backgroundPosition = "center";
-  subjectnamedev.style.borderRadius = "10px";
+ subjectnamedev.style.borderTopLeftRadius = "7px";
+subjectnamedev.style.borderTopRightRadius = "7px";
+
   subjectnamedev.innerHTML = `
-   <div class="courses flex flex-col items-start p-10 min-h-[150px] m-0">
-     <h3 class=" no fw-600 text-xl text-start min-h-10" style='color: rgb(0, 0, 163)' >${subject.subjectName}</h3>
-      <h3 class=" no text-xs line-1-6  "  style="color:white; padding-top:7px ">${subject.description}</h3>
-   </div>
-  
- 
+   <div class="courses w-full flex flex-col items-start p-10 min-h-[250px] m-0">
+    </div>
+    
   `;
+  let pargarph = document.createElement("div");
+  pargarph.className = ` flex flex-col items-start p-10 gap-2`;
+  pargarph.innerHTML = `     
+     <h3 class=" h31 text-xl text-start min-h-10"  >${subject.subjectName}</h3>
+      <h3 class=" h32  line-1-6  "  padding-top:7px ">${subject.description}</h3>`;
 
   card.appendChild(subjectnamedev);
+  card.appendChild(pargarph);
 
   const btn = document.createElement("div");
   btn.className =
-    " flex justify-center start-btn mt-10 fw-500 c-blue pointer p-6";
+    "startbtn flex justify-center start-btn mt-10 fw-500 c-blue pointer p-6";
   const startbtn = document.createElement("button");
   startbtn.className = " start ";
 
-if(currentUser.role==='teacher'){
-  startbtn.innerText = "Add Questions";
-}
+  if (currentUser.role === "teacher") {
+    startbtn.innerText = "Add Questions";
+  }
 
-if(currentUser.role ==='student'){
-  startbtn.innerText = "Start Exam";
-
-}
+  if (currentUser.role === "student") {
+    startbtn.innerText = "Start Exam";
+  }
 
   startbtn.addEventListener("click", () => subjectButtonHandler(subject.id));
 
@@ -84,10 +88,9 @@ if(currentUser.role ==='student'){
   container.appendChild(card);
 });
 
-
 //handle log out btn click
-logoutBtn.addEventListener('click',()=>{
+logoutBtn.addEventListener("click", () => {
   logout();
-  localStorage.removeItem('userId');
+  localStorage.removeItem("userId");
   window.location.href = `/`;
-})
+});
